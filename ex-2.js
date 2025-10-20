0a3422fb6af548c5d61f1aeb2ec5d79dc5a56412
gitgitja <samoyhoiyai@gmail.com>
2025-10-21 01:33:42 +0700

    assignment conditional logic

--- ex-2.js
+++ ex-2.js
@@ -2,3 +2,14 @@
 let lightBulbStatus = "On";
 
 // Start coding here.
+if (lightBulbStatus==="On"){
+   lightBulbStatus = "Light bulb is On."
+} else if (lightBulbStatus==="Off") {
+   lightBulbStatus = "Light bulb is Off"
+} else if (lightBulbStatus==="Broker"){
+   lightBulbStatus = "Light bulb is Broker."
+} else {
+   lightBulbStatus = "Please choose the correct input (On/Off/Broken)"
+}
+
+console.log(lightBulbStatus);
--- ex-4.js
+++ ex-4.js
@@ -3,3 +3,18 @@
 let lightBulbStatus = "On";
 
 //Start coding here
+switch (lightBulbStatus){
+    case lightBulbStatus="On":
+    lightBulbStatus = "Light bulb is On."
+    break;
+    case lightBulbStatus="Off": 
+    lightBulbStatus = "Light bulb is Off"
+    break;
+    case lightBulbStatus="Broken":
+    lightBulbStatus = "Light bulb is Broken."
+    break;
+    default:
+    lightBulbStatus = "Please choose the correct input (On/Off/Broken)"
+ }
+ 
+ console.log(lightBulbStatus);
